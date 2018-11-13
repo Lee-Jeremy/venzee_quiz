8fb6fd1f121ec74153d9f9ff58c4889998bc8d1c
lee-jeremy <jeremylee.business@gmail.com>
2018-11-13 07:42:37 -0800

    expandNamespace.test.js filled in with unit tests.

--- spec/expandNamespace.test.js
+++ spec/expandNamespace.test.js
@@ -1,22 +1,67 @@
-const proxyquire = require( 'proxyquire' );
+const sinon = require('sinon');
+const proxyquire = require('proxyquire');
+const expect = require('chai').expect;
 
-describe( 'The ./lib/expandNamespaces function', ()=>{
+var fakeLoadNamespace;
 
-  it( 'should keep paths without namespaces unchanged', ()=>{
+describe('The ./lib/expandNamespaces function', () => {
+  beforeEach(() => {
+    fakeLoadNamespace = sinon.stub();
+  })
 
-    const expandNamespaces = proxyquire( '../lib/expandNamespace', {
+  it('should keep paths without namespaces unchanged', () => {
+
+    const expandNamespaces = proxyquire('../lib/expandNamespace', {
       './loadNamespaces': sinon.stub()
-    } );
+    });
 
     const expected = './somePath';
-    const actual   = expandNamespaces( expected );
+    const actual = expandNamespaces(expected);
 
-    expect( actual ).to.equal( expected );
+    expect(actual).to.equal(expected);
 
-  } );
+  });
 
-  it.skip( 'start writing tests here', ()=>{
+  it.skip('start writing tests here', () => {
 
-  } );
+  });
 
-} );
+  it('should notify that the namespace does not exist with an error message', () => {
+    const modulePath = '<someLib>/someModule';
+    const callerPath = 'someModule';
+
+    const expandNamespaces = proxyquire('../lib/expandNamespace', {
+      './loadNamespaces': fakeLoadNamespace.returns({ namespaces: { otherLib: 'otherLib' } })
+    });
+
+    function getErrorMessage() {
+      try {
+        return expandNamespaces(modulePath, callerPath);
+      } catch (e) {
+        return e.message
+      }
+    }
+
+    const expected = 'namespace <someLib> is not defined.';
+    const actual = getErrorMessage();
+
+    expect(actual).to.equal(expected)
+
+  });
+
+  it('should change a namespace into a valid path', () => {
+    const modulePath = '<someLib>/someModule';
+    const callerPath = 'someModule';
+
+    const expandNamespaces = proxyquire('../lib/expandNamespace', {
+      './loadNamespaces': fakeLoadNamespace.returns({ namespaces: { someLib: 'someLib' } })
+    })
+
+    const expected = '..\\someLib\\someModule';
+    const actual = expandNamespaces(modulePath, callerPath);
+
+    expect(actual).to.equal(expected)
+
+  });
+
+});
